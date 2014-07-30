7deeae3c5e7c4b69984d2029669b1293881b2b76
Markus Wersig <markus.wersig@gmail.com>
2014-07-30 15:45:24 +0200

    Added Reference for NBT Tag Names

---
+++ src/main/java/de/mpw/betterbeacon/reference/Names.java
@@ -0,0 +1,25 @@
+package de.mpw.betterbeacon.reference;
+
+/**
+ * Created by Markus on 30.07.2014.
+ */
+public class Names {
+    public static class NBT {
+        public static final String ITEMS = "Items";
+        public static final String KNOWLEDGE = "Knowledge";
+        public static final String CHARGE_LEVEL = "chargeLevel";
+        public static final String MODE = "mode";
+        public static final String CRAFTING_GUI_OPEN = "craftingGuiOpen";
+        public static final String UUID_MOST_SIG = "UUIDMostSig";
+        public static final String UUID_LEAST_SIG = "UUIDLeastSig";
+        public static final String DISPLAY = "display";
+        public static final String COLOR = "color";
+        public static final String STATE = "teState";
+        public static final String CUSTOM_NAME = "CustomName";
+        public static final String DIRECTION = "teDirection";
+        public static final String OWNER = "owner";
+        public static final String OWNER_UUID_MOST_SIG = "ownerUUIDMostSig";
+        public static final String OWNER_UUID_LEAST_SIG = "ownerUUIDLeastSig";
+        public static final String BEACON_EFFECT = "effectPrimary";
+    }
+}
